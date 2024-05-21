bda3c3aa40d8bceb08bb453fa9f9365be0ac920f
Pablo Stafforini <pablo@stafforini.com>
2024-05-21 13:25:17 -0300

    Update .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+((markdown-mode . ((jinx-languages . "es")
+		   (flycheck-languagetool-language . "es")
+		   (jinx-local-words . "Does Is Objections act and authors avorecer chap ctuar description draft false generalesl gradientBottom gradientTop how immorally means menu next objection page permissive tell title to type ualquiera us utilitarianism weight"))))
