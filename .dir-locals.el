698fb814298c9554ab3b94bf3bca3c2bb630da1b
Pablo Stafforini <pablo@stafforini.com>
2024-05-21 14:36:22 -0300

    Update .dir-locals.el: add local words

--- .dir-locals.el
+++ .dir-locals.el
@@ -2,4 +2,4 @@
 ;;; For more information see (info "(emacs) Directory Variables")
 ((markdown-mode . ((jinx-languages . "es")
 		   (flycheck-languagetool-language . "es")
-		   (jinx-local-words . "Does Is Objections act and authors avorecer chap ctuar description draft false generalesl gradientBottom gradientTop how immorally means menu next objection page permissive tell title to type ualquiera us utilitarianism weight"))))
+		   (jinx-local-words . "Does Is Objections act and authors avorecer chap ctuar description draft false generalesl gradientBottom gradientTop how immorally means menu next objection page permissive tell title to type ualquiera us utilitarianism weight descripcion acting"))))
